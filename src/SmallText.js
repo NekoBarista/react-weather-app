dc596258945571c32459672e8f772363a008e854
NekoBarista <100394344+NekoBarista@users.noreply.github.com>
2022-05-16 16:40:48 +1000

    small text update

---
+++ src/SmallText.js
@@ -0,0 +1,9 @@
+function SmallText () {
+    <div className="SmallText">
+       Open Source Code by <a href='https://github.com/NekoBarista/react-weather-app'> Vicki Smith </a> 
+    </div>
+
+
+}
+
+export default SmallText
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals'
+import SmallText from './SmallText'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <SmallText />
     </React.StrictMode>
 );
 
