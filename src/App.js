01091eb195759d3ac95db7fa8b98e32087e1b4ea
NekoBarista <100394344+NekoBarista@users.noreply.github.com>
2022-05-16 16:11:00 +1000

    initial helloworld and UI

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">    
+     <h1>  Melbourne </h1>
+    
+     <p> Monday, 16th of May</p>
+     <h2> 30°C</h2>
+     <h3> Cloudy </h3>
+     <form>
+       <input type='text'placeholder="Enter a City" className="searchBox"></input> 
+       <input type='submit' className="btn btn-primary" value="Search"></input>
+     </form>
+    </div>
+  );
+}
+
+export default App;
