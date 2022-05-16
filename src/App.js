7fd03afd5d15b0dd7b1230afdc8a0af701b54f11
NekoBarista <100394344+NekoBarista@users.noreply.github.com>
2022-05-17 01:25:07 +1000

    revert

--- src/App.js
+++ src/App.js
@@ -1,43 +1,21 @@
 import './App.css';
 import React, { useState } from 'react';
-import axios from 'axios';
+
+
 
 function App() {
-let [city, setCity] = useState("Melbourne");
-let [submit,setSubmit] = useState(false);
-let [display, setDisplay] =useState(false);
-  
-function searchCity(event) {
-  setSubmit(false);
-  setCity(event.target.value);   
 
-  }
-
-  function displayWeather(event) {
-    event.preventDefault();
-    getWeather()
-
-  }
-  
-  function showWeather(response){
-    setSubmit(true);
-    setDisplay(response.data.name)
-  }
-  function getWeather() {
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=9bf2efd0f6df71b36df66a55219d0c2d
-  `;
-  axios.get(url).then(showWeather);}
-
+ 
   return (
     <div className="App">    
-     <h1>  {display} </h1>
-     <p> Monday, 16th of May</p>
-     <p> 16.20pm</p>
-<img src="https://cdn.icon-icons.com/icons2/2505/PNG/512/sunny_weather_icon_150663.png" alt="sunny icon"/>
-     <h2> 30°C</h2>
+     <h1>  Melbourne </h1>
+     <p> Thursday, May 17th, 2022 </p>
+     <p> 1.18am</p>
+     <img src="https://cdn.icon-icons.com/icons2/2505/PNG/512/sunny_weather_icon_150663.png" alt="cloudy"/>
+     <h2> 30° C / F </h2>
      <h3> Cloudy </h3>
-     <form className="cityForm" onSubmit={displayWeather}>
-       <input type='text'placeholder="Enter a City" className="searchBox" onChange={searchCity}></input> 
+     <form className="cityForm">
+       <input type='text'placeholder="Enter a City" className="searchBox"></input> 
        <input type='submit' className="btn btn-primary" value="Search"></input>
      </form>
  
@@ -45,10 +23,9 @@ function searchCity(event) {
          Open Source Code by <a href='https://github.com/NekoBarista/react-weather-app'> Vicki Smith </a> 
       </div>
       </div>
-  
-  
-
   );
 }
+  
+
 
 export default App;
