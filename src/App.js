ca8e8794e0ac87784ad1ffeb4f86e632777003af
NekoBarista <100394344+NekoBarista@users.noreply.github.com>
2022-05-16 21:14:17 +1000

    beginningsearchengine
    had to stop, plane catching time!

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
 import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+let [city, setCity] = useState("Melbourne");
+let [submit,setSubmit] = useState(false);
+let [display, setDisplay] =useState(false);
+  
+function searchCity(event) {
+  setSubmit(false);
+  setCity(event.target.value);   
+
+  }
+
+  function displayWeather(event) {
+    event.preventDefault();
+    getWeather()
+
+  }
+  
+  function showWeather(response){
+    setSubmit(true);
+    setDisplay(response.data.name)
+  }
+  function getWeather() {
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=9bf2efd0f6df71b36df66a55219d0c2d
+  `;
+  axios.get(url).then(showWeather);}
+
   return (
     <div className="App">    
-     <h1>  Melbourne </h1>
+     <h1>  {display} </h1>
      <p> Monday, 16th of May</p>
      <p> 16.20pm</p>
 <img src="https://cdn.icon-icons.com/icons2/2505/PNG/512/sunny_weather_icon_150663.png" alt="sunny icon"/>
      <h2> 30°C</h2>
      <h3> Cloudy </h3>
-     <form>
-       <input type='text'placeholder="Enter a City" className="searchBox"></input> 
+     <form className="cityForm" onSubmit={displayWeather}>
+       <input type='text'placeholder="Enter a City" className="searchBox" onChange={searchCity}></input> 
        <input type='submit' className="btn btn-primary" value="Search"></input>
      </form>
  
