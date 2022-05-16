d6b26e10848e7e9dd8232a20a0ec0efdf35f1c77
NekoBarista <100394344+NekoBarista@users.noreply.github.com>
2022-05-16 16:37:34 +1000

    updates

--- src/smalltext.js
+++
@@ -1,7 +0,0 @@
-export default function SmallText () {
-    return (
-    <div className="SmallText">
-       <a href="https://github.com/NekoBarista/react-weather-app"> Open Source Code </a> by Vicki Smith
-    </div>
-    )
-}
