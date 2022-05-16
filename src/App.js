b9c58955ff895f65dc65a1fb23167b9dd9f1a3c5
NekoBarista <100394344+NekoBarista@users.noreply.github.com>
2022-05-16 17:07:28 +1000

    icon addition

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
      <h1>  Melbourne </h1>
      <p> Monday, 16th of May</p>
      <p> 16.20pm</p>
+<img src="https://cdn.icon-icons.com/icons2/2505/PNG/512/sunny_weather_icon_150663.png" alt="sunny icon"/>
      <h2> 30°C</h2>
      <h3> Cloudy </h3>
      <form>
@@ -13,7 +14,7 @@ function App() {
        <input type='submit' className="btn btn-primary" value="Search"></input>
      </form>
  
-      <div className="SmallText">
+      <div className="smallText">
          Open Source Code by <a href='https://github.com/NekoBarista/react-weather-app'> Vicki Smith </a> 
       </div>
       </div>
